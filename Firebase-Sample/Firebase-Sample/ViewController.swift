8f1f376d554b5a3089bd61a868325864a60150f5
Vitor neves <j.victtor.neves@hotmail.com>
2019-11-15 22:35:20 -0300

    add project

---
+++ Firebase-Sample/Firebase-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase-Sample
+//
+//  Created by joao.vitor.f.naves on 15/11/19.
+//  Copyright © 2019 vitor.neves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
